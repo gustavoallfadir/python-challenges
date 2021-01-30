15d15d9291941efd36934c07bd40d34db7ed63c8
Gustavo Allfadir <gustavo.allfadir@gmail.com>
2021-01-30 14:14:34 -0600

    one more

---
+++ playing_digits.py
@@ -0,0 +1,30 @@
+'''Some numbers have funny properties. For example:
+
+    89 --> 8¹ + 9² = 89 * 1
+
+    695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+    46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+Given a positive integer n written as abcd... (a, b, c, d... being digits) 
+and a positive integer p we want to find a positive integer k, if it exists, 
+such as the sum of the digits of n taken to the successive powers of p is equal to k * n.
+
+In other words:
+
+Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be given as strictly positive integers.
+'''
+
+def dig_pow(n, p):
+    if sum(pow(int(j),p+i) for i,j in enumerate(str(n))) % n == 0:
+        return sum(pow(int(j),p+i) for i,j in enumerate(str(n)))/n
+    return -1
+
+print(dig_pow(89, 1)) #returns 1
+print(dig_pow(92, 1)) #returns -1
+print(dig_pow(695, 2)) #returns 2
+print(dig_pow(46288, 3)) #returns 51
