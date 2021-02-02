55acc07330437ce79815a591eded6b393e2ef3d4
Gustavo Allfadir <gustavo.allfadir@gmail.com>
2021-02-02 01:17:34 -0600

    fix

--- oneliners.py
+++ oneliners.py
@@ -8,10 +8,11 @@ s = list(map(lambda n: sum(n) if sum(n) > 0 else 'Zero', tuples))
 
 print(s)
 
-inst = '''Write a function that searchs for a word inside a string and
-returns a text slice with a range of +20 and -20 characters from
+instruction = '''Write a function that searchs for a word inside a string and
+returns a text slice with a range of +15 and -15 characters from
 the point of the searched word'''
 
-find = lambda s, q: s[s.find(q)-20:s.find(q)+20 if q in s else None]
+find = lambda s, q: '...' + s[s.find(q)-15:s.find(q)+15] + '...' if q in s else None
 
-print(find(inst, 'slice'))
+print(find(instruction, 'searchs'))
+print(find(instruction,'range'))
